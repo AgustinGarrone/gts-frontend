19141df852cbc748e989c25aa3a1cd336256cf2d
unknown <agustinngarrone@hotmail.com>
2024-06-24 17:17:25 -0300

    styles: notifications

--- src/screen/home/components/notifications/index.tsx
+++ src/screen/home/components/notifications/index.tsx
@@ -1,6 +1,25 @@
+"use client";
+import { useGetUserNotifications } from "@/hooks/useNotificationClient";
+import { Notification } from "@/types/models";
+import { NotificationCard } from "@/ui/components/notificationCard";
 import { Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+// @ts-ignore
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { IoNotificationsCircle } from "react-icons/io5";
 
 export const Notifications = () => {
+  const [notifications, setNotifications] = useState<
+    Notification[] | undefined
+  >([]);
+  const { data, isLoading, error, isRefetching } = useGetUserNotifications();
+
+  useEffect(() => {
+    console.log(data);
+    setNotifications(data);
+  }, [data]);
+
   return (
     <Flex
       w="100%"
@@ -10,8 +29,47 @@ export const Notifications = () => {
       backdropFilter="blur(2px)"
       boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)"
       border="1px solid rgba(255, 255, 255, 0.18)"
+      alignItems="center"
     >
-      <Text m="1em 0 0 3em">últimas notificaciones</Text>
+      <Flex
+        bgGradient="linear(to-b, #e6f7ff, #b3ecff)"
+        h="100%"
+        alignItems="center"
+        justifyContent="flex-start"
+      >
+        <IoNotificationsCircle color="black" size="40px" />
+      </Flex>
+
+      <Flex cursor="pointer">
+        {isLoading ? (
+          <Text mt="1em">Cargando...</Text>
+        ) : notifications && notifications.length > 0 ? (
+          <Splide
+            options={{
+              perPage: 3,
+              perMove: 1,
+              gap: "3em",
+              arrows: false,
+              width: "70vw", // Ajusta la altura según tus necesidades
+              type: "slide",
+              height: "15vh",
+            }}
+          >
+            {notifications.map((notification) => (
+              <SplideSlide key={notification.id}>
+                <NotificationCard
+                  id={notification.id}
+                  message={notification.message}
+                  userId={notification.userId}
+                />
+              </SplideSlide>
+            ))}{" "}
+            *
+          </Splide>
+        ) : (
+          <Text mt="1em">No hay notificaciones disponibles.</Text>
+        )}
+      </Flex>
     </Flex>
   );
 };
---
+++ src/ui/components/notificationCard/index.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from "@chakra-ui/react"
+import { FC } from "react"
+
+type NotificationProps = {
+    id: number;
+    userId: number;
+    message: string;
+  }
+  
+  export const NotificationCard: FC<NotificationProps> = ({ id, userId, message }) => {
+    return (
+      <Flex direction="column" ml="2em" h="100%" bgColor="darkblue" p="5px" borderRadius="15px" w="15em" alignItems="center" justifyContent="center">
+        <Text color="white" textAlign="center" fontSize="xx-small">{message}</Text>
+      </Flex>
+    );
+  };
